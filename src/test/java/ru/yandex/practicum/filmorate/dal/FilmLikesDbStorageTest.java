8b9b97532ae0182f9c50e455121e4b5c1a8170f1
OldOak <artxakrus@yandex.ru>
2026-05-19 19:30:49 +0400

    Исправлены и добавлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/dal/FilmLikesDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/FilmLikesDbStorageTest.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.MpaRating;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.*;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.filmLikes.FilmLikesDbStorage;
 import ru.yandex.practicum.filmorate.storage.mappers.FilmRowMapper;
@@ -39,7 +37,7 @@ class FilmLikesDbStorageTest {
     // Проверка подсчёта лайков фильма
     @Test
     void shouldCountFilmLikes() {
-        Film film = createTestFilm("Film");
+        Film film = createTestFilm("Film", 1, LocalDate.of(2000, 12, 1));
         Film createdFilm = filmStorage.create(film);
 
         Long userId = createTestUser("user@email.ru", "user");
@@ -51,11 +49,19 @@ class FilmLikesDbStorageTest {
         assertThat(likesCount).isEqualTo(1);
     }
 
-    // Проверка получения популярных фильмов
+    // Проверка получения популярных фильмов без указания жанра и года
     @Test
-    void shouldFindTopFilmsByLikes() {
-        Film film1 = filmStorage.create(createTestFilm("Film1"));
-        Film film2 = filmStorage.create(createTestFilm("Film2"));
+    void shouldFindTopFilmsByLikesWithoutGenreAndYear() {
+        Film film1 = filmStorage.create(createTestFilm(
+                "Film1",
+                1,
+                LocalDate.of(2000, 12, 1)
+        ));
+        Film film2 = filmStorage.create(createTestFilm(
+                "Film2",
+                2,
+                LocalDate.of(2000, 12, 1)
+        ));
 
         Long user1Id = createTestUser("user1@email.ru", "user1");
         Long user2Id = createTestUser("user2@email.ru", "user2");
@@ -64,22 +70,106 @@ class FilmLikesDbStorageTest {
         filmStorage.addLike(film2.getId(), user1Id);
         filmStorage.addLike(film2.getId(), user2Id);
 
-        List<Long> topFilmIds = filmLikesStorage.findTopFilmsByLikes(10);
+        List<Long> topFilmIds = filmLikesStorage.findTopFilmsByLikes(10, null, null);
+
+        assertThat(topFilmIds).containsExactly(film2.getId(), film1.getId());
+    }
+
+    // Проверка получения популярных фильмов с указанием жанра и года
+    @Test
+    void shouldFindTopFilmsByLikesWithGenreAndYear() {
+        Film film1 = filmStorage.create(createTestFilm(
+                "Film1",
+                1,
+                LocalDate.of(2000, 12, 1)
+        ));
+        Film film2 = filmStorage.create(createTestFilm(
+                "Film2",
+                2,
+                LocalDate.of(2000, 12, 1)
+        ));
+
+        Long user1Id = createTestUser("user1@email.ru", "user1");
+        Long user2Id = createTestUser("user2@email.ru", "user2");
+
+        filmStorage.addLike(film1.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user2Id);
+
+        List<Long> topFilmIds = filmLikesStorage.findTopFilmsByLikes(10, 1, 2000);
+        List<Long> topFilmIds2 = filmLikesStorage.findTopFilmsByLikes(10, 2, 2000);
+
+        assertThat(topFilmIds).containsExactly(film1.getId());
+        assertThat(topFilmIds2).containsExactly(film2.getId());
+    }
+
+    // Проверка получения популярных фильмов с указанием года без жанра
+    @Test
+    void shouldFindTopFilmsByLikesWithYearNoGenre() {
+        Film film1 = filmStorage.create(createTestFilm(
+                "Film1",
+                1,
+                LocalDate.of(2000, 12, 1)
+        ));
+        Film film2 = filmStorage.create(createTestFilm(
+                "Film2",
+                2,
+                LocalDate.of(2000, 12, 1)
+        ));
+
+        Long user1Id = createTestUser("user1@email.ru", "user1");
+        Long user2Id = createTestUser("user2@email.ru", "user2");
+
+        filmStorage.addLike(film1.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user2Id);
+
+        List<Long> topFilmIds = filmLikesStorage.findTopFilmsByLikes(10, null, 2000);
+
+        assertThat(topFilmIds).containsExactly(film2.getId(), film1.getId());
+    }
+
+    // Проверка получения популярных фильмов с указанием жанра без года
+    @Test
+    void shouldFindTopFilmsByLikesWithGenreNoYear() {
+        Film film1 = filmStorage.create(createTestFilm(
+                "Film1",
+                1,
+                LocalDate.of(2000, 12, 1)
+        ));
+        Film film2 = filmStorage.create(createTestFilm(
+                "Film2",
+                1,
+                LocalDate.of(1999, 12, 1)
+        ));
+
+        Long user1Id = createTestUser("user1@email.ru", "user1");
+        Long user2Id = createTestUser("user2@email.ru", "user2");
+
+        filmStorage.addLike(film1.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user1Id);
+        filmStorage.addLike(film2.getId(), user2Id);
+
+        List<Long> topFilmIds = filmLikesStorage.findTopFilmsByLikes(10, 1, null);
 
         assertThat(topFilmIds).containsExactly(film2.getId(), film1.getId());
     }
 
     // Создание фильма с корректными данными
-    private Film createTestFilm(String name) {
+    private Film createTestFilm(String name, int genreId, LocalDate releaseDate) {
         Film film = new Film();
         film.setName(name);
         film.setDescription("Description");
-        film.setReleaseDate(LocalDate.of(2000, 1, 1));
+        film.setReleaseDate(releaseDate);
         film.setDuration(120);
 
         MpaRating mpaRating = new MpaRating();
         mpaRating.setId(1);
         film.setMpa(mpaRating);
+
+        Genre genre = new Genre();
+        genre.setId(genreId);
+        film.setGenres(List.of(genre));
 
         return film;
     }
