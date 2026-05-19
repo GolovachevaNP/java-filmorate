a31afb44be6a0d40088afbcadaedc582ed60fbdb
OldOak <artxakrus@yandex.ru>
2026-05-19 23:03:51 +0400

    Удалена лишняя запись жанров в БД

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -34,18 +34,6 @@ public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
                 film.getDuration(), film.getMpa().getId());
 
         film.setId(id);
-
-        // Запись жанров в БД
-        jdbc.batchUpdate(
-                "INSERT INTO film_genres (film_id, genre_id) VALUES (?, ?)",
-                film.getGenres(),
-                film.getGenres().size(),
-                (ps, genre) -> {
-                    ps.setLong(1, id);
-                    ps.setInt(2, genre.getId());
-                }
-        );
-
         return film;
     }
 
